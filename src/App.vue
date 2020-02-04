48568ff15758d317e83f0f46b97cf34e88289b1b
EricWangUTM <yifeng.wang@mail.utoronto.ca>
2020-02-04 17:43:06 -0500

    Displaying UofT on wrld 3D

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    
+    <!--<script src="https://cdn-webgl.wrld3d.com/wrldjs/dist/latest/wrld.js"></script>dir-->
+    <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.1/leaflet.css" rel="stylesheet" />-->
+
+    <img src="./assets/UofT.png">
+
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <div id="map" style="height: 500px;"></div>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.1/leaflet.css" rel="stylesheet" />
+
+    <!--<h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+      <li><a href="https://leafletjs.com/" target="_blank">Leaflet</a></li>
+      <li><a href="https://www.wrld3d.com/" target="_blank">WRLD</a></li>
+      <li><a href="https://www.wrld3d.com/legal" target="_blank">Partners</a></li>
+    </ul>-->
+
   </div>
 </template>
 
 <script>
+const Wrld = require("wrld.js")
 export default {
   name: 'app',
+  mounted() {
+    var map = L.Wrld.map("map", "3cd9616c750d2d11c80f85d0a2cf32f0", {
+      center: [43.549, -79.6636],
+      zoom: 15
+    });
+
+    //setTimeout(function() {
+      // zooms in on a very precise area
+      /* TODO: get precise coordinates of DH
+      */
+      //var bounds = L.latLngBounds([43.54, -79.66], [43.54, -79.66]);
+      //map.fitBounds(bounds);
+    //}, 3000);
+  },
+  
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: ''
     }
-  }
+  },
 }
 </script>
 
@@ -39,22 +56,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
-  color: #42b983;
+  color: #033267;
 }
 </style>
